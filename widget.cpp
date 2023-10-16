d73d3b2b7622ef5fcfd456fd344e3bcfe51a711b
hBuzzy <47574560+hBuzzy@users.noreply.github.com>
2023-10-16 17:32:27 +0300

    Removed style stings

--- widget.cpp
+++ widget.cpp
@@ -55,18 +55,6 @@ Widget::Widget(QWidget *parent) : QWidget(parent), ui(new Ui::Widget) {
           &Enemy::OnDamageButtonClicked);
   connect(enemy, &Enemy::MakeDamage, player, &Player::TakeDamage);
   connect(player, &Player::HealthChanged, healthBar, &QProgressBar::setValue);
-
-  const QString dangerStyle =
-      "QProgressBar::chunk {background: #F44336; Width: 20px; margin: 0.5px;"
-      "border: 1px solid black; border-radius:8px; Border-Radius: 4px;} "
-      "QProgressBar { text-align: center; font-size:14px; border-radius:8px; "
-      "color:black;}";
-
-  const QString normalStyle =
-      "QProgressBar::chunk {background: #009688; Width: 20px; margin: 0.5px; "
-      "border: 1px solid black; border-radius:8px; Border-Radius: 4px;} "
-      "QProgressBar { text-align: center; font-size:14px; border-radius:8px; "
-      "color:black;}";
 }
 
 Widget::~Widget() { delete ui; }
